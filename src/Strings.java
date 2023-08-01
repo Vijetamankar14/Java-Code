33a7e3387a0442230e6cb50d4d48d77a6adb63cb
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-01 21:04:15 +0530

    String Replacement

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+//WAP to replace the specified character of a string with character "a"
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String s1=("Raja Ram Mohan Roy");
+        s1 = s1.replace("R", "A");
+
+        System.out.println(s1);
+
+    }
+}
