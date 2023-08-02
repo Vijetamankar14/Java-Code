561830b1b0bb31855229723db87a54b7d1c9a487
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-02 19:53:56 +0530

    String Replacement

---
+++ src/String_hello.java
@@ -0,0 +1,9 @@
+//WAP to replace "HELLO" with "HAI"
+
+public class String_hello {
+    public static void main(String[] args) {
+        String s1 = "HELLO";
+        s1=s1.replaceAll("HELLO","HAIII");
+        System.out.println(s1);
+    }
+}
--- src/Strings.java
+++ src/Strings.java
@@ -1,12 +1,21 @@
 //WAP to replace the specified character of a string with character "a"
 
+import java .util.Scanner;
 public class Strings {
 
     public static void main(String[] args) {
-        String s1=("Raja Ram Mohan Roy");
-        s1 = s1.replace("R", "A");
 
-        System.out.println(s1);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter String Name : ");
+         String name = scan.nextLine();
+        System.out.println("Enter the alphabet which have to replace ");
+        String s1 = scan.next();
+        System.out.println("Replace with which alphabet");
+        String s2 = scan.next();
+        String name1 =name.replace("s1","s2");
+
+
+        System.out.println(name1);
 
     }
 }
